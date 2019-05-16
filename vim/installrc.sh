e437e86da8fbc80f290b92ce6a349fe674d8a938
anton <hjjg200@gmail.com>
2019-05-16 16:19:12 +0900

    Update installrc.sh

--- vim/installrc.sh
+++ vim/installrc.sh
@@ -2,6 +2,15 @@
 curl -s https://raw.githubusercontent.com/hjjg200/hjjg200/master/vim/.vimrc?t=$(date +%s) > ~/.vimrc
 vimrc=$(cat ~/.vimrc)
 
+# Substitue Register in .vimrc
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    reg='"p' # p register for linux
+else
+    reg='"*' # Clipboard
+fi
+vimrc=$(eval echo "$(echo $vimrc)")
+
+# If 256 color supported
 if [ "$TERM" = 'xterm-256color' ]; then
 
     if [ ! -d "$HOME/.vim" ]; then
