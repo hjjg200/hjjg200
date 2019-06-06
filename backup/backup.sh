a40819f5b8b0bc3aa154fb34e5d0600a92d457c2
Anton J <hjjg200@gmail.com>
2019-06-07 03:06:21 +0900

    fixes

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Functions
+elementBelongs () {
+    local e=$1
+    shift
+    for i; do
+        [[ "$i" -eq "$e" ]] && return 0
+    done
+    return 1
+}
+
 # Check
 if [[ ! "$#" -eq 2 ]]; then
     echo "Usage: backup.sh <file_path> <destination_directory>"
@@ -8,9 +18,28 @@ if [[ ! "$#" -eq 2 ]]; then
     exit 1
 fi
 
+# Prepare
+bcs=${BACKUP_CHECKSUM,,}
+if [[ "$bcs" -eq "sha1" ]]; then
+    csapp=sha1sum
+    csext=.sha1
+elif [[ "$bcs" -eq "sha256" ]]; then
+    csapp=sha256sum
+    csext=.sha256
+else
+    caspp=md5sum
+    csext=.md5
+fi
+
 # Backup
 filename=$1
 destDir=$2
+
+# Checksum
+csstr=`$csapp $filename`
+cmd="scp $filename$csext $BACKUPHOST:$destDir"
+$BACKUPPATH/bin/log.sh "$cmd"
+$cmd
 
 # Execute and log
 cmd="scp $filename $BACKUPHOST:$destDir"
