746f639e63235b17b9fbe5707a1bd7330acf4e4f
Anton J <hjjg200@gmail.com>
2019-06-08 05:33:10 +0900

    fix

--- backup/sanity.sh
+++ backup/sanity.sh
@@ -6,8 +6,8 @@
 [[ ! -d "$BACKUPPATH/bin" ]] && exit 1
 [[ ! -d "$BACKUPPATH/log" ]] && exit 1
 
-[[ "$BACKUPPATH" -eq "" ]] && exit 1
-[[ "$BACKUPHOST" -eq "" ]] && exit 1
-[[ "$BACKUPDEST" -eq "" ]] && exit 1
+[[ "$BACKUPPATH" == "" ]] && exit 1
+[[ "$BACKUPHOST" == "" ]] && exit 1
+[[ "$BACKUPDEST" == "" ]] && exit 1
 
 exit 0
